fb8ca28ed7dc3263d922620c732e76b177275839
Andreas Moll <Andreas.Moll@synchrotron.org.au>
2018-05-18 17:15:46 +1000

    Read config values from environment variables

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=dmg-tracking.py
--- config.py
+++ config.py
@@ -1,13 +1,21 @@
 import os
+from tzlocal import get_localzone
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-phrase'
+    TIMEZONE = get_localzone()
+
+    PORTAL_SETTINGS = {
+        'host': os.environ.get('PORTAL_HOST') or 'localhost',
+        'client': os.environ.get('PORTAL_CLIENT') or None,
+        'password': os.environ.get('PORTAL_PASSWORD') or None,
+    }
 
     MONGODB_SETTINGS = {
-        'db': 'data_mgmt',
-        'host': 'localhost',
-        'port': 27017
+        'db': os.environ.get('MONGODB_DB') or 'data_mgmt',
+        'host': os.environ.get('MONGODB_HOST') or 'localhost',
+        'port': os.environ.get('MONGODB_PORT') or 27017,
     }
 
     SWAGGER = {
