54716fdb67eaffe453d834750d5fb89255000417
Andreas Moll <Andreas.Moll@synchrotron.org.au>
2018-05-22 17:30:40 +1000

    Use default attribute in config and convert mongodb port to integer

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ from tzlocal import get_localzone
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-phrase'
+    SECRET_KEY = os.environ.get('SECRET_KEY', default='super-secret-phrase')
     TIMEZONE = get_localzone()
 
     PORTAL_SETTINGS = {
-        'host': os.environ.get('PORTAL_HOST') or 'localhost',
-        'client': os.environ.get('PORTAL_CLIENT') or None,
-        'password': os.environ.get('PORTAL_PASSWORD') or None,
+        'host': os.environ.get('PORTAL_HOST', default='localhost'),
+        'client': os.environ.get('PORTAL_CLIENT', default=None),
+        'password': os.environ.get('PORTAL_PASSWORD', default=None),
     }
 
     MONGODB_SETTINGS = {
-        'db': os.environ.get('MONGODB_DB') or 'data_mgmt',
-        'host': os.environ.get('MONGODB_HOST') or 'localhost',
-        'port': os.environ.get('MONGODB_PORT') or 27017,
+        'db': os.environ.get('MONGODB_DB', default='data_mgmt'),
+        'host': os.environ.get('MONGODB_HOST', default='localhost'),
+        'port': int(os.environ.get('MONGODB_PORT', default=27017)),
     }
 
     SWAGGER = {
